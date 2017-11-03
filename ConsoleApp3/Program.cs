b88df580edd1305b46b08c2ce0fe747391d42dca
CSharpFri <CSharpFri@CLASS-2-07>
2017-11-03 19:29:50 +0100

    oprava

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,12 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
-      
+    class Program
+    {
         static void Main(string[] args)
 
         {
+            Dom mojDom = new Dom("Activ 1", "aktivni", "nie", 3, 30.9f, 74.8f);
+            mojDom.PoskytniInformace();
+
             Console.WriteLine("Hello!");
-            
+            Console.ReadLine();
+
+
         }
+
+
+
     }
 }
